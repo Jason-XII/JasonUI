9c2f85b60da0a4507641e871fd3cffdffc4e6769
Jason-XII <65878611+Jason-XII@users.noreply.github.com>
2021-05-05 09:31:50 +0800

    fixed bug in layouts

--- src/layouts.py
+++ src/layouts.py
@@ -1,27 +1,25 @@
 from PySide2.QtWidgets import QHBoxLayout, QVBoxLayout, QLayout, QWidget
 
 
-class Group:
-    def __init__(self, *widgets):
+class HorizontalGroup(QHBoxLayout):
+    def __init__(self, *widgets, parent=None):
+        super().__init__(self, parent)
         for widget in widgets:
             if isinstance(widget, int):
                 self.addStretch(widget)
             elif isinstance(widget, QLayout):
-                w = QWidget(parent)
-                w.setLayout(widget)
-                self.addWidget(w)
+                self.addLayout(widget)
             else:
-                self.addWidget(w)
+                self.addWidget(widget)
 
 
-class HorizontalGroup(QHBoxLayout, Group):
+class VerticalGroup(QVBoxLayout):
     def __init__(self, *widgets, parent=None):
-        super().__init__(self, parent)
-        super().__init__(self, *widgets)
-
-
-class VerticalGroup(QVBoxLayout, Group):
-    def __init__(self, *widgets, parent=None):
-        super().__init__(self, parent)
-        super().__init__(self, *widgets)
-        
+        super().__init__(parent)
+        for widget in widgets:
+            if isinstance(widget, int):
+                self.addStretch(widget)
+            elif isinstance(widget, QLayout):
+                self.addLayout(widget)
+            else:
+                self.addWidget(widget)
